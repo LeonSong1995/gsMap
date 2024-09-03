6eb2e02bf7531d644eff6923323906645b971ca6
Ganten-Hornby <65263129+Ganten-Hornby@users.noreply.github.com>
2024-09-03 13:42:57 +0800

    temp diagnosis module

--- src/gsMap/diagnosis.py
+++ src/gsMap/diagnosis.py
@@ -7,12 +7,14 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 from pandas.api.types import is_numeric_dtype
+from plotly.subplots import make_subplots
 from scipy.stats import norm
 import scanpy as sc
 import pyranges as pr
 from gsMap import generate_ldscore
 import gsMap.config
 from gsMap.utils.manhattan_plot import ManhattanPlot
+from gsMap.visualize import draw_scatter, load_st_coord
 
 # %%
 warnings.filterwarnings("ignore", category=FutureWarning)
@@ -74,12 +76,14 @@ def generate_manhattan_plot(config: DiagnosisConfig):
     gwas_data = convert_z_to_p(gwas_data)
 
     # Load the H5AD file and gene marker scores
+    logger.info('Loading ST data...')
     adata = sc.read_h5ad(config.input_hdf5_path, backed='r')
     mk_score = pd.read_feather(config.mkscore_feather_file)
     mk_score.set_index('HUMAN_GENE_SYM', inplace=True)
     mk_score = mk_score.T
 
     # Load LDSC results
+    logger.info('Loading Spatial LDSC results...')
     trait_ldsc_result = load_ldsc(f"{config.input_ldsc_dir}/{config.sample_name}_{config.trait_name}.csv.gz")
     mk_score_aligned = mk_score.loc[trait_ldsc_result.index]
 
@@ -124,15 +128,12 @@ def generate_manhattan_plot(config: DiagnosisConfig):
     bim_pr = pr.PyRanges(bim_pr)
 
     # Get SNP-gene pairs using the loaded LDSC Boost instance
-    SNP_gene_pair_gtf = s_ldsc_boost.get_SNP_gene_pair_from_gtf(bim, bim_pr)
-    SNP_gene_pair = SNP_gene_pair_gtf
+    logger.info('Creating SNP-gene pairs...')
+    SNP_gene_pair = s_ldsc_boost.get_SNP_gene_pair_from_gtf(bim, bim_pr)
 
     # Merge GWAS data with SNP-gene pairs
     gwas_data_with_gene = gwas_data.merge(SNP_gene_pair, on='SNP', how='inner')
     gwas_data_with_gene.rename(columns={'gene_name': 'GENE'}, inplace=True)
-
-    # Merge with PCC
-    gwas_data_with_gene = gwas_data_with_gene.merge(corr, left_on='GENE', right_index=True, how='left')
 
     # Group the marker score by annotation and determine the highest GSS annotation
     grouped_mk_score = mk_score_aligned.groupby(adata.obs[config.annotation]).median()
@@ -147,6 +148,8 @@ def generate_manhattan_plot(config: DiagnosisConfig):
     # Filter out genes with Max Score < 1.0
     high_GSS_Gene_annotation_pair = high_GSS_Gene_annotation_pair[high_GSS_Gene_annotation_pair['Median_GSS'] >= 1.0]
 
+    # Merge with PCC
+    high_GSS_Gene_annotation_pair = high_GSS_Gene_annotation_pair.merge(corr, left_on='Gene', right_index=True, )
 
     # Merge GWAS data with gene annotations
     gwas_data_with_gene_annotation = gwas_data_with_gene.merge(
@@ -156,9 +159,23 @@ def generate_manhattan_plot(config: DiagnosisConfig):
         how='left'
     )
 
-    gwas_data_with_gene_annotation['Annotation_text']='PCC: ' + gwas_data_with_gene_annotation['PCC'].round(2).astype(str) + '<br>' + 'Annotation: ' + gwas_data_with_gene_annotation['Annotation']
+    # Save the gene diagnostic information
+    save_dir = Path(config.diagnosis_save_dir)
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    gene_diagnostic_info_cols = ['Gene', 'Annotation', 'Median_GSS', 'PCC']
+    gene_diagnostic_info = gwas_data_with_gene_annotation[gene_diagnostic_info_cols].drop_duplicates().dropna(
+        subset=['Gene'])
+    gene_diagnostic_info.sort_values('PCC', ascending=False, inplace=True)
+    gene_diagnostic_info_save_path = save_dir / f'{config.sample_name}_{config.trait_name}_Gene_Diagnostic_Info.csv'
+    gene_diagnostic_info.to_csv(gene_diagnostic_info_save_path, index=False)
+    logger.info(f'Gene diagnostic information saved to {gene_diagnostic_info_save_path}.')
+
+    gwas_data_with_gene_annotation['Annotation_text'] = 'PCC: ' + gwas_data_with_gene_annotation['PCC'].round(2).astype(
+        str) + '<br>' + 'Annotation: ' + gwas_data_with_gene_annotation['Annotation']
 
     # Create the Manhattan plot
+    logger.info('Generating Diagnostic Manhattan Plot...')
     fig = ManhattanPlot(
         dataframe=gwas_data_with_gene_annotation,
         title='gsMap Diagnosis Manhattan Plot',
@@ -169,16 +186,74 @@ def generate_manhattan_plot(config: DiagnosisConfig):
     )
 
     fig.show()
-    save_dir = Path(config.diagnosis_save_dir)
     save_manhattan_plot_path = save_dir / f'{config.sample_name}_{config.trait_name}_Diagnostic_Manhattan_Plot.html'
     fig.write_html(save_manhattan_plot_path)
+    logger.info(f'Diagnostic Manhattan Plot saved to {save_manhattan_plot_path}. Open in browser to view.')
+
+
+def generate_GSS_distribution(config: DiagnosisConfig):
+    save_dir = Path(config.diagnosis_save_dir)
+
+    # Load the H5AD file and gene marker scores
+    logger.info('Loading ST data...')
+    adata = sc.read_h5ad(config.input_hdf5_path, )
+
+    logger.info('Loading marker score...')
+    mk_score = pd.read_feather(config.mkscore_feather_file)
+    mk_score.set_index('HUMAN_GENE_SYM', inplace=True)
+    mk_score = mk_score.T
+
+    assert config.selected_genes is not None, 'Please provide selected genes for GSS distribution plot'
+
+    common_genes = set(adata.var_names) & set(config.selected_genes)
+    if len(common_genes) == 0:
+        raise Warning(
+            "Selcted genes don't contain any gene in the dataset: "
+        )
+    elif len(common_genes) < len(config.selected_genes):
+        warnings.warn(
+            f"Some genes don't contain in the dataset: {set(config.selected_genes) - common_genes}"
+        )
+
+    plot_genes = config.selected_genes
+
+    sub_fig_save_dir = save_dir / 'sub_figures'
+    sub_fig_save_dir.mkdir(parents=True, exist_ok=True)
+
+    for selected_gene in plot_genes:
+        logger.info(f'Generating GSS & Expression distribution plot for {selected_gene}...')
+
+        # Gene Expression Distribution
+        expression = adata[:, selected_gene].X.toarray().flatten()
+        expression_series = pd.Series(expression, index=adata.obs.index, name='Expression')
+        select_gene_expression_with_space_coord = load_st_coord(adata, expression_series, 'annotation')
+        sub_fig_1 = draw_scatter(select_gene_expression_with_space_coord,
+                                 title=f'{selected_gene} (Expression)',
+                                 annotation='annotation',
+                                 color_by='Expression',
+                                 point_size=2
+                                 )
+        save_sub_fig_1_path = sub_fig_save_dir / f'{config.sample_name}_{selected_gene}_Expression_Distribution.html'
+        sub_fig_1.write_html(str(save_sub_fig_1_path))
+
+        # GSS distribution
+        select_gene_GSS_with_space_coord = load_st_coord(adata, mk_score[selected_gene].rename('GSS'), 'annotation')
+        sub_fig_2 = draw_scatter(select_gene_GSS_with_space_coord,
+                                 title=f'{selected_gene} (GSS)',
+                                 annotation='annotation',
+                                 color_by='GSS',
+                                 point_size=2
+                                 )
+        save_sub_fig_2_path = sub_fig_save_dir / f'{config.sample_name}_{selected_gene}_GSS_Distribution.html'
+        sub_fig_2.write_html(str(save_sub_fig_2_path))
+
 
 
 # %%
 if __name__ == '__main__':
     config = DiagnosisConfig(
         sample_name='E16.5_E1S1.MOSTA',
-        input_hdf5_path='/mnt/e/0_Wenhao/7_Projects/20231213_GPS_Liyang/test/20240902_gsMap_Local_Test/example_data/ST/E16.5_E1S1.MOSTA.h5ad',
+        input_hdf5_path='/mnt/e/0_Wenhao/7_Projects/20231213_GPS_Liyang/test/20240902_gsMap_Local_Test/E16.5_E1S1.MOSTA/find_latent_representations/E16.5_E1S1.MOSTA_add_latent.h5ad',
         annotation='annotation',
         mkscore_feather_file='/mnt/e/0_Wenhao/7_Projects/20231213_GPS_Liyang/test/20240902_gsMap_Local_Test/E16.5_E1S1.MOSTA/latent_to_gene/E16.5_E1S1.MOSTA_gene_marker_score.feather',
         bfile_root='/mnt/e/0_Wenhao/7_Projects/20231213_GPS_Liyang/test/20240902_gsMap_Local_Test/GPS_resource/LD_Reference_Panel/1000G_EUR_Phase3_plink/1000G.EUR.QC',
@@ -192,4 +267,6 @@ if __name__ == '__main__':
         diagnosis_save_dir='/mnt/e/0_Wenhao/7_Projects/20231213_GPS_Liyang/test/20240902_gsMap_Local_Test/E16.5_E1S1.MOSTA/diagnosis'
     )
 
-    generate_manhattan_plot(config)
+    # generate_manhattan_plot(config)
+
+    generate_GSS_distribution(config)
